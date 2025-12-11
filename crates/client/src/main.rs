e306815b1307f0a182322098b9b6962581304d24
ExoticSliceOfPizza <136937744+PizzaLvr49@users.noreply.github.com>
2025-12-10 22:28:12 -0500

    basic window

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::{
+    prelude::*,
+    window::{PresentMode, WindowMode},
+};
+
+fn main() -> AppExit {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                mode: WindowMode::BorderlessFullscreen(MonitorSelection::Current),
+                present_mode: PresentMode::AutoVsync,
+                ..default()
+            }),
+            ..default()
+        }))
+        .run()
 }
